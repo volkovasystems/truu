6dd977f2e9885ca8aa180c9277b5953de31eb431
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-10 16:04:23 +0800

    Added compact build procedure.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 "use strict";
 
 const webpack = require( "webpack" );
-const ResolverPlugin = webpack.ResolverPlugin;
-const DirectoryDescriptionFilePlugin = ResolverPlugin.DirectoryDescriptionFilePlugin;
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 module.exports = {
 	"entry": "./truu.support.js",
 	"resolve": {
-		"modulesDirectories": [ "bower_components", "node_modules" ]
+		"descriptionFiles": [
+			"bower.json",
+			"package.json"
+		],
+		"modules": [
+			"bower_components",
+			"node_modules"
+		],
+		"mainFields": [
+			"support",
+			"browser",
+			"module",
+			"main"
+		]
 	},
 	"module": {
-		"preLoaders": [
+		"rules": [
 			{
+				"enforce": "pre",
 				"test": /\.support\.js$/,
 				"loader": "source-map-loader"
 			}
@@ -24,8 +36,6 @@ module.exports = {
 		"filename": "truu.deploy.js"
 	},
 	"plugins": [
-		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
-		new ResolverPlugin( new DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
 		new UglifyJsPlugin( {
 			"compress": {
 				"keep_fargs": true,
@@ -37,5 +47,5 @@ module.exports = {
 			"mangle": false
 		} )
 	],
-	"devtool": "#inline-source-map"
+	"devtool": "#cheap-module-inline-source-map"
 };
