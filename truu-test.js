ba6139dd803379af6e99a8e0b5df31cfc8f6d982
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-02 13:18:44 +0800

    Updated test.

--- truu-test.js
+++ truu-test.js
@@ -1,9 +1,12 @@
 "use strict";
 
+const assert = require( "assert" );
 const truu = require( "./truu.js" );
 
-console.log( truu( { "name": "simple" } ) );
-console.log( truu( [ 1, 2, 3 ] ) );
-console.log( truu( [ { } ] ) );
-console.log( truu( { } ) );
-console.log( truu( [ ] ) );
+assert.equal( truu( { "name": "simple" } ), true, "should be true" );
+assert.equal( truu( [ 1, 2, 3 ] ), true, "should be true" );
+assert.equal( truu( [ { } ] ), true, "should be true" );
+assert.equal( truu( { } ), false, "should be false" );
+assert.equal( truu( [ ] ), false, "should be false" );
+
+console.log( "ok" );
